d8ae2834f29f1a25c546984b76e8ce7f445517b9
m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br <m4p.ednaldo.dilorenzo@polodeinovacao.ifpb.edu.br>
2025-11-17 21:02:06 -0300

    Adicionada funcionalidade de salar produtos e listar produtos salvos.

--- ecommerce-mongo-redis/build.gradle.kts
+++ ecommerce-mongo-redis/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 
 dependencies {
     implementation("org.mongodb:mongodb-driver-sync:5.2.0")
+    implementation("redis.clients:jedis:5.1.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
