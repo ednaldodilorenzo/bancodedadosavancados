b2dc9b2ad6275450f09ccb8f0ddab3b3ce422491
edsf <ednaldo.dilorenzo@gmail.com>
2026-03-02 20:00:55 -0300

    Adição do jpa, configurações do banco e hibernate e classe main.

--- 202601/jpacrud/src/main/java/org/example/Main.java
+++ 202601/jpacrud/src/main/java/org/example/Main.java
@@ -1,17 +1,41 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        try {
+            while (true) {
+                System.out.print("""
+                        Menu:
+                           1) Inserir Usuário
+                           2) Listar Usuários Cadastrados
+                           3) Buscar Usuário por ID
+                           4) Atualizar Usuário
+                           5) Excluir Usuário
+                           0) Sair
+                        opção: """);
+                BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+                int opcao = Integer.parseInt(bf.readLine());
+                switch (opcao) {
+                    case 1 -> System.out.println("Salvar usuário");
+                    case 2 -> System.out.println("Listar Usuários");
+                    case 3 -> System.out.println("Buscar usuário por id");
+                    case 4 -> System.out.println("Atualizar usuário");
+                    case 5 -> System.out.println("Excluir usuário");
+                    case 0 -> {
+                        break;
+                    }
+                    default ->
+                        System.out.println("Opção inválida");
+                }
+            }
+        } catch (IOException ioex) {
+            ioex.printStackTrace();
         }
     }
 }
