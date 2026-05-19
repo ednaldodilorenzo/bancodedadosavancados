ea87a46c8e70445a979e353c92b639e4172c6b9b
edsf <ednaldo.dilorenzo@gmail.com>
2026-05-18 21:59:14 -0300

    Projeto de prática com mongodb e Redis.

--- 202601/praticamongocacheaside/src/main/java/com/poc/crud/service/ProdutoService.java
+++ 202601/praticamongocacheaside/src/main/java/com/poc/crud/service/ProdutoService.java
@@ -1,10 +1,15 @@
 package com.poc.crud.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poc.crud.dao.ProdutoDao;
 import com.poc.crud.model.Produto;
+import redis.clients.jedis.Jedis;
 
 public class ProdutoService {
     private final ProdutoDao produtoDao;
+    private final Jedis jedis = new Jedis("localhost", 6379);
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public ProdutoService(ProdutoDao produtoDao) {
         this.produtoDao = produtoDao;
@@ -14,7 +19,14 @@ public class ProdutoService {
         this.produtoDao.salvar(produto);
     }
 
-    public Produto buscarPorId(String id) {
-        return this.produtoDao.buscarPorId(id);
+    public Produto buscarPorId(String id) throws JsonProcessingException {
+        String chave = "produto:" + id;
+        String cache = jedis.get(chave);
+        if (cache != null) {
+            return objectMapper.readValue(cache, Produto.class);
+        }
+        var produto = this.produtoDao.buscarPorId(id);
+        jedis.setex(chave, 3600, objectMapper.writeValueAsString(produto));
+        return produto;
     }
 }
